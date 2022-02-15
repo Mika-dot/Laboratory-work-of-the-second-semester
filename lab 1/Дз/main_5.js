065fa8c4ebf052877bfc8137fe0a317835a3edcb
Serebrovsky Sergey Maksimovich <61110689+Mika-dot@users.noreply.github.com>
2022-02-15 15:35:40 +0500

    Add files via upload

---
+++ lab 1/Дз/main_5.js
@@ -0,0 +1,8 @@
+let NumberOne = prompt('Число один', 1);
+let NumberTwo = prompt('Число два', 2);
+
+if((Number(NumberOne) == 10) || (Number(NumberTwo) == 10) || (Number(NumberTwo + NumberOne) == 10)){
+    alert("истина ");
+}else {
+    alert("лож");
+}
