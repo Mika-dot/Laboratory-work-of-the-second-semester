d26d7f29d93e88726041437e5fd9f6d0fd566ff0
Serebrovsky Sergey Maksimovich <61110689+Mika-dot@users.noreply.github.com>
2022-03-01 12:18:02 +0500

    Update main_4.js

--- lab 2/дз/main_4.js
+++ lab 2/дз/main_4.js
@@ -3,3 +3,7 @@ let student = {
     last_name: "Иванов",
     first_name: "Иван"
     };
+    
+    console.log(Object.keys(student));
+    //Студент Имя Фамилия учится в номер группе
+    console.log("Студент " + student["first_name"] + " " + student["last_name"] + " учится в " + student["group"] + " группе");
