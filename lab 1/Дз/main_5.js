ac3f5814116aedc55d70ff9b2f424b77b7db8456
Serebrovsky Sergey Maksimovich <61110689+Mika-dot@users.noreply.github.com>
2022-03-01 11:15:01 +0500

    Update main_5.js

--- lab 1/Дз/main_5.js
+++ lab 1/Дз/main_5.js
@@ -4,5 +4,7 @@ let NumberTwo = Number(prompt('Число два', 2));
 if((NumberOne == 10) || (NumberTwo == 10) || (NumberTwo + NumberOne == 10)){
     alert("истина ");
 }else {
-    alert("лож");
+    alert("ложь");
 }
+
+console.log((NumberOne == 10 | NumberTwo == 10 | NumberOne + NumberTwo == 10) ? "истина" : "ложь");
